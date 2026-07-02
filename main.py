8c19972ac75c1f466627b35b490f1aa5fde7fd32
weyimi barrow <oritseweyinmibarrow@gmail.com>
2026-07-02 10:37:17 -0600

    added API for more currencies, inverse for loop to fix bug

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-EXCHANGE_RATES = {"USD": 1, "NGN": 0.00072, "CAD": 0.70, "GBP": 1.33}
+import requests
+response = requests.get("https://open.er-api.com/v6/latest/USD")
+print(f"Status: {response.status_code}")
+data = response.json()
+
+raw_rates = data["rates"]
+inverted_rates = {}
+for currency, rate in raw_rates.items():
+    inverted_rates[currency] = 1 / rate
+
+
+EXCHANGE_RATES = inverted_rates 
+
 
 def convert_currencies(amount_to_convert, from_currency, to_currency):
     amount_in_usd = amount_to_convert * EXCHANGE_RATES[from_currency]
     converted_amount = amount_in_usd / EXCHANGE_RATES[to_currency]
     return converted_amount
 
-
-
-EXCHANGE_RATES["EUR"] = 1.14
+example_1 = convert_currencies(50, "EUR", "AUD")
+example_2 = convert_currencies(200, "UGX", "USD")
+print(example_1)
+print(example_2)
